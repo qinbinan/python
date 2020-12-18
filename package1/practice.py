7132b34519a27ac35b65f838ef3c363b73180ce0
qinbinan <584595770@qq.com>
2020-12-18 14:36:37 +0800

    181436

---
+++ package1/practice.py
@@ -0,0 +1,22 @@
+# -*-coding:UTF-8 -*-
+# 求1-100的
+
+# summ = 0
+# for i in range(101):
+#     summ += i
+# print(summ)
+# 
+# 
+# num_list=[]
+# for i in range(101):
+#     if i % 3 == 0 and i % 5 != 0:
+#         num_list.append(i)
+# print(num_list)
+
+def yolo():
+    num_list = []
+    for i in range(101):
+        if i % 3 == 0 and i % 5 != 0:
+            num_list.append(i)
+    print("100以内能被3整除但是不能被5整除的数:%s" % num_list)
+    
