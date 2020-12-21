73dd1108a12cdad14ec0746a6f0794321453e53a
qinbinan <584595770@qq.com>
2020-12-21 14:28:37 +0800

    211428

---
+++ demo/demo.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from functions.demo1 import addGood
+
+print('测试python')
+
+addGood()
---
+++ functions/testfunctions.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from functions.demo1 import addGood
+
+
+addGood()
