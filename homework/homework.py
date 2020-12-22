5afea9d206e9b6be366799e6edf3455a140ac679
qinbinan <584595770@qq.com>
2020-12-22 21:05:05 +0800

    222105

--- homework/homework.py
+++ homework/homework.py
@@ -10,23 +10,19 @@
 # kuan = 5
 # mianji = chang * kuan
 # print(mianji)
-
 # months_sal = 10000 + 200
 # print(months_sal)
 # print(9 / 3)
 # print(10 // 3)
 # print(10 % 3)
 # print(10**3)
-
 # sal=3000
 # comm=400
 # if sal>2000 and comm>300:
 #     print('good')
-
 # sal=5000
 # if not sal<=4000:
 #     print('ok')
-
 # 20 int
 # 3.1415  float
 # True
@@ -34,11 +30,11 @@
 # None
 # name='byron'
 # name="byron"
-userinfo1=()
-userinfo2=(1,'byron','china',20,100000,None)
-userinfo3=((1,'byron','china',20,100000,None),
-          (2,'smith','USA',23,70000,None),
-          (3,'scott','USA',25,40000,None))
+# userinfo1=()
+# userinfo2=(1,'byron','china',20,100000,None)
+# userinfo3=((1,'byron','china',20,100000,None),
+#           (2,'smith','USA',23,70000,None),
+#           (3,'scott','USA',25,40000,None))
 # print(userinfo2[5])
 # print(userinfo2[0:3])
 # print(userinfo2[-3:-1])
@@ -46,9 +42,108 @@ userinfo3=((1,'byron','china',20,100000,None),
 # print(userinfo2[-3:])
 # print(userinfo2[2:-2])
 # print(userinfo2[-2:2])
-for i in range(len(userinfo2)):
-    print(userinfo2[i])
-
+# for i in range(len(userinfo2)):
+#     print(userinfo2[i])
+# for i in range(len(userinfo3)):
+#     for j in range(len(userinfo3[i])):
+#         print(userinfo3[i][j])
+# empinfo=['蜀国','吴国','魏国']
+# empinfo.append('群雄')
+# print(empinfo)
+# empinfo.insert(3, '争霸')
+# print(empinfo)
+# empinfo.pop()
+# print(empinfo)
+# empinfo.pop(3)
+# print(empinfo)
+# empinfo.remove('争霸')
+# print(empinfo)
+# empinfo.clear()
+# print(empinfo)
+# age=int(input('年龄:'))
+# if age>18:
+#     print('成年')
+# elif age>14:
+#     print('童工')
+# else:
+#     print('未满14周岁')
+# verify=input('验证码：')
+# username=input('用户名：')
+# password=input('密码：')
+# 
+# if verify=='587772':
+#     if username=='byron' and password=='19940402':
+#         print('登录成功')
+#     else:
+#         print('用户名或密码错误')
+# else:
+#     print('验证码错误')
+# empinfo = {
+#     'empno':7788,
+#     'ename':'smith',
+#     'job':'clerk',
+#     'sal':800,
+#     'cost':['桑拿', '采耳', '搓澡']
+#     }
+# print(empinfo['empno'])
+# print(empinfo.get('empno'))
+# result = {
+#     "resultcode": "200",
+#     "reason": "Return Successd!",
+#     "result": {
+#         "province": "重庆",
+#         "city": "重庆",
+#         "areacode": "023",
+#         "zip": "404100",
+#         "company": "联通",
+#         "card": "18602355173"
+#     },
+#     "error_code": 0
+# }
+# print(result['result']['province'])
+# print(result['resultcode'])
+# 
+# print(list(result['result'].keys()))
+# ename='smith'
+# print(type(ename))
+# sal=2000
+# print(type(sal))
+# print('我是：%s,我的月薪：%s'% (ename,sal))
+# def addGood():
+#     print('hi')
+# addGood()
+# def total():
+#     total = 0
+#     for i in range(1, 101):
+#         total = total + i
+#     print('和：%s'% total)
+# total()
+# def div():
+#     llist=[]
+#     for i in range(1,101):
+#         if i%3==0 and i%5!=0:
+#             llist.append(i)
+#     print(llist)
+# div()
+# def times():
+#     total=0
+#     for i in range(15,50,4):
+#         if i <=50:
+#             total=total+1
+#     print(total)
+# times()
+# def guess():
+#     me=input('让别人猜：')
+#     print(me)
+#     for i in range(100):
+#         guess=input('输入：')
+#         if guess>me:
+#             print('大')
+#         elif guess<me:
+#             print('小')
+#         else:
+#             print('猜对了')
+#             break
 
 
 
