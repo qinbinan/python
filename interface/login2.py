ad83e22cfc597e0306d244c14709d0c24381ff0b
qinbinan <584595770@qq.com>
2021-01-04 15:30:57 +0800

    041530

--- interface/login2.py
+++ interface/login2.py
@@ -9,14 +9,15 @@ class TestEdc:
 
     def test_login(self):
         url = 'http://192.168.1.5:8080/recruit.students/login/in'
+        logger.info('ulr地址为：' + url)
         params = {
             'account':'admin',
             'pwd':'660B8D2D5359FF6F94F8D3345698F88C'
         }
-        cookies = {
+        cookies={
             'JSESSIONID':'53F7CEDF986BC9223EAFD91348A400BA',
         }
-        response = get(url, params, cookies=cookies)
+        response = get(url, params,cookies=cookies)
         logger.info('状态码为%s' % response.status_code)
         assert response.status_code == 200
 
