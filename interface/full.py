318c5563427a5f327b7b8e776c531981c673f394
qinbinan <584595770@qq.com>
2021-01-04 15:37:04 +0800

    041536

--- interface/full.py
+++ interface/full.py
@@ -21,7 +21,6 @@ class TestEdc:
          功能：登录
     
     '''
-
     def test_login(self):
         url = '%s:%s/recruit.students/login/in' % (self.env['ip'], self.env['port'])
         params = {
@@ -31,11 +30,11 @@ class TestEdc:
         response = get(url, params, cookies=self.cookies)
         assert response.status_code == 200
     
+    
     '''
          功能：新建学校
     
     '''
-
     def test_asi(self):
         url = '%s:%s/recruit.students/school/manage/addSchoolInfo' % (self.env['ip'], self.env['port'])
         data = {
@@ -47,55 +46,55 @@ class TestEdc:
         response = post(url, data=data, cookies=self.cookies)
         assert response.status_code == 200 and (loads(response.text))['code'] == 1
     
+    
     '''
          功能：设置学生录入时间
     
     '''
-
     def test_ssr(self):
         url = '%s:%s/recruit.students/school/manage/setStudentRecruitTime' % (self.env['ip'], self.env['port'])
         json = [{"id":"4610", "recruitStartTime":"2021-01-05", "recruitEndTime":"2021-01-07", "isStudentRecruitTime":"1"}]
         response = post(url, None, json=json, cookies=self.cookies, headers=self.headers)
         assert response.status_code == 200 and (loads(response.text))['code'] == 1
     
+    
     '''
          功能：设置招生录取时间
     
     '''
-
     def test_set(self):
         url = '%s:%s/recruit.students/school/manage/setEnrollmentTime' % (self.env['ip'], self.env['port'])
         json = [{"id":"4610", "startTime":"2020-12-28", "endTime":"2020-12-31", "isSelfEnrollmentTime":"1"}]
         response = post(url, None, json=json, cookies=self.cookies, headers=self.headers)
         assert response.status_code == 200 and (loads(response.text))['code'] == 1
     
+    
     '''
          功能：重置密码
     
     '''
-
     def test_rp(self):
         url = '%s:%s/recruit.students/school/manage/resetPassword' % (self.env['ip'], self.env['port'])
         json = [{"id":"475413", "schoolId":"4610"}]
         response = post(url, None, json=json, cookies=self.cookies, headers=self.headers)
         assert response.status_code == 200 and (loads(response.text))['code'] == 1
     
+    
     '''
          功能：启用
     
     '''
-
     def test_es(self):
         url = '%s:%s/recruit.students/school/manage/enableOrDisableSchool' % (self.env['ip'], self.env['port'])
         json = [{"id":"475413", "disable":1, "schoolId":"4610"}]
         response = post(url, None, json=json, cookies=self.cookies, headers=self.headers)
         assert response.status_code == 200 and (loads(response.text))['code'] == 1
     
+    
     '''
          功能：禁用
     
-    '''    
-
+    '''
     def test_ds(self):
         url = '%s:%s/recruit.students/school/manage/enableOrDisableSchool' % (self.env['ip'], self.env['port'])
         json = [{"id":"475413", "disable":0, "schoolId":"4610"}]
